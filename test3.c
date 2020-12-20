5cd91a04da1ba33b412b3a04e7b2a772ba4f985e
tagynedlrb <tagynedlrb@naver.com>
2020-12-20 18:55:39 +0900

    redirection, pipe working. Need to fix background processing, exit etc.

--- test3.c
+++ test3.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <fcntl.h>
 #define READ 0
 #define WRITE 1
 #define MAXLINE 100
@@ -20,6 +21,12 @@ int main(void){
 
 		char str[1024];
 		char *command1, *command2;
+		int fd;
+
+		// for exec
+		char *args[64];
+		char **next = args;
+
 
 		printf("[myshell]");
 		fgets(str, sizeof(str), stdin);
@@ -29,74 +36,109 @@ int main(void){
 		int status, count = 0;
 		signal(SIGCHLD, childsignal);
 
-	//	read()
-
 		// check is Background
-		if(1)	//has & )
+		if(strchr(str,'&') != NULL){
+			*(strchr(str, '&')) = '\0';
 			amper = 1;
+		}
 		else
 			amper = 0;
 
 
 		/*launch process*/
 		if((pid1 = fork()) == 0){
-/*
-			if(strchr(str,'>') != NULL){	// input redirection
-				command1 = strtok(str, ">");
-				command2 = strtok(NULL, ">");
+			command1 = str;
+printf("command : %s\n", command1);
+
+			/* Input redirection*/
+			if(strchr(str,'<') != NULL){
+				command1 = strtok(str, "< ");
+				command2 = strtok(NULL, "< ");
+				fd = open(command2, O_CREAT|O_TRUNC|O_WRONLY, 0600);
+				dup2(fd, 0);
+				close(fd);
+			}
+
+			/* Output redirection*/
+			if(strchr(str,'>') != NULL){
+				command1 = strtok(str, "> ");
+				command2 = strtok(NULL, "> ");
 				fd = open(command2, O_CREAT|O_TRUNC|O_WRONLY, 0600);
 				dup2(fd, 1);
 				close(fd);
-			if(strchr(str,'<') != NULL){	// output redirection
-				command1 = strtok(str, "<");
-				command2 = strtok(NULL, "<");
-				fd = open(command2, O_CREAT|O_TRUNC|O_WRONLY, 0600);
-				dup2(fd, 1);
-				close(fd);
-	
-				fd = create(newfile)
-			if(strchr(str,'|') != NULL){	// use pipe
+			}
+
+			/* Use pipe*/
+			if(strchr(str,'|') != NULL){
 				int fd[2];
 
 				command1 = strtok(str, "|");
 				command2 = strtok(NULL, "|");
 				pipe(fd);
-				if((pid2 = fork()) == 0){
+				if(fork() == 0){
 					close(fd[READ]);
 					dup2(fd[WRITE], 1);
 					close(fd[WRITE]);
-					execlp(command1, command1, NULL);
-					perror("pipe);
+
+					/* executing routine*/
+					char *temp = strtok(command1, " \n");
+					while(temp != NULL){
+						*next++ = temp;
+						//printf("%s\n", temp);
+						temp = strtok(NULL, " \n");
+					}
+					*next = NULL;
+
+					for(next = args; *next != 0; next++)
+						//puts(*next);
+					execvp(args[0], args);
+					perror("pipe");
 				}
 				else{
 					close(fd[WRITE]);
 					dup2(fd[READ], 0);
 					close(fd[READ]);
-					execlp(command2, command2, NULL);
-					perror("pipe);
+
+					/* executing routine*/
+					char *temp = strtok(command2, " \n");
+					while(temp != NULL){
+						*next++ = temp;
+						//printf("%s\n", temp);
+						temp = strtok(NULL, " \n");
+					}
+					*next = NULL;
+
+					for(next = args; *next != 0; next++)
+						//puts(*next);
+					execvp(args[0], args);
+					perror("pipe");
 				}
-			}
-			else{	// Not PIPE
+			} // end pipe
 
-
-				execlp(command1, command1, NULL);
+			/* executing routine*/
+			char *temp = strtok(command1, " \n");
+			while(temp != NULL){
+				*next++ = temp;
+				//printf("%s\n", temp);
+				temp = strtok(NULL, " \n");
 			}
-*/
+			*next = NULL;
+
+			for(next = args; *next != 0; next++)
+				//puts(*next);
+			execvp(args[0], args);
 		}
 
 		// wait if foreground process
-//		if(amper == 0)
-//			wait();
+		if(amper == 0)
+			wait(&status);
 
-		/*launch background process*/	
-
-		
 		while(amper == 1){
 			pause();
 			printf("Wakened in pause()! %d\n", ++count);
 		}
 
-		printf("bye\n");
+//		printf("bye\n");
 	}
 }
 
